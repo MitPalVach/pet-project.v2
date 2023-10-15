700a796f0199b3a842f90ad16f984a663bbada1d
Dmitriy Vachugov <mitpal16@gmail.com>
2023-10-15 21:02:16 +0400

    add modal, portal, change header;

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,4 @@
 import React, { Suspense } from 'react';
-import './styles/index.scss';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTheme } from 'app/providers/ThemeProvider';
 import { AppRouter } from 'app/providers/router';
@@ -12,11 +11,14 @@ function App() {
   return (
     <div className={ classNames( 'app', {}, [ theme ] ) }>
       <Suspense fallback="">
+
         <Navbar/>
+
         <div className="content-page">
           <Sidebar/>
           <AppRouter/>
         </div>
+
       </Suspense>
     </div>
   );
