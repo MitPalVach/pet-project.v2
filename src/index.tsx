383bf8e691ba336764bd43f7beef39b0cf5c6f7b
Dmitriy Vachugov <mitpal16@gmail.com>
2023-10-16 21:12:27 +0400

    fix theme;

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,7 @@
-// eslint-disable-next-line react/no-deprecated
-import { StoreProvider } from 'app/providers/StoreProvider';
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'app/providers/ThemeProvider';
+import { StoreProvider } from 'app/providers/StoreProvider';
 import App from './app/App';
 import 'app/styles/index.scss';
 import './shared/config/i18n/i18n';
