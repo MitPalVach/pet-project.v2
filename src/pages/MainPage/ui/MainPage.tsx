e85d8d19609ad3b67ce0d1058c0dd1446be90cf4
Dmitriy Vachugov <mitpal16@gmail.com>
2023-10-30 21:28:54 +0400

    add new theme, react refresh plugin;

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,16 +1,20 @@
-import React, { memo } from 'react';
+import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { Counter } from 'entities/Counter';
+import { Input } from 'shared/ui/Input/Input';
 
-const MainPage = memo( () => {
+const MainPage = () => {
   const { t } = useTranslation();
+  const [ value, setValue ] = useState( '' );
+
+  const onChange = ( val: string ) => {
+    setValue( val );
+  };
 
   return (
     <div>
-      <Counter />
       { t( 'Главная страница' ) }
     </div>
   );
-} );
+};
 
 export default MainPage;
